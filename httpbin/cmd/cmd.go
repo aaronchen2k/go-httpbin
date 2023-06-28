d9cc78a1ec506da67379ddb9aca28ba4dde34598
aaron <462826@qq.com>
2023-06-28 20:30:59 +0800

    add more than one interfaces in same endpoint

--- httpbin/cmd/cmd.go
+++ httpbin/cmd/cmd.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	defaultListenHost = "0.0.0.0"
-	defaultListenPort = 8080
+	defaultListenPort = 50300
 
 	// Reasonable defaults for our http server
 	srvReadTimeout       = 5 * time.Second
